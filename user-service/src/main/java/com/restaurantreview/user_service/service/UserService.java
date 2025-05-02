190dce32c86a705e05c85b1a68c4461d231c24e4
Gianmarco Setzu <gianmarco.setzu@esterni.unipolsai.it>
2025-05-02 19:34:08 +0200

    [USER-SERVICE] - Add jwt authentication

--- user-service/src/main/java/com/restaurantreview/user_service/service/UserService.java
+++ user-service/src/main/java/com/restaurantreview/user_service/service/UserService.java
@@ -3,9 +3,12 @@ package com.restaurantreview.user_service.service;
 import com.restaurantreview.user_service.dto.UserDTO;
 import com.restaurantreview.user_service.dto.UserLoginRequest;
 import com.restaurantreview.user_service.dto.UserRegisterRequest;
+import com.restaurantreview.user_service.exception.EmailAlreadyExistsException;
+import com.restaurantreview.user_service.exception.UserNotRegisteredException;
 import com.restaurantreview.user_service.model.UserAccount;
 import com.restaurantreview.user_service.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -15,36 +18,35 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public UserAccount registerUser(UserRegisterRequest request) {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public UserDTO registerUser(UserRegisterRequest request) {
         userRepository.findByEmail(request.getEmail())
                 .ifPresent(user -> {
-                    throw new RuntimeException("Email already registered");
+                    throw new EmailAlreadyExistsException("Email already registered");
                 });
 
         UserAccount newUser = new UserAccount();
         newUser.setUsername(request.getUsername());
         newUser.setEmail(request.getEmail());
-        newUser.setPassword(request.getPassword());
 
-        return userRepository.save(newUser);
+        String hashedPassword = passwordEncoder.encode(request.getPassword());
+        newUser.setPassword(hashedPassword);
+
+        userRepository.save(newUser);
+
+        return new UserDTO(newUser.getId(), newUser.getEmail(), newUser.getUsername());
     }
 
     public UserDTO loginUser(UserLoginRequest request) {
         Optional<UserAccount> foundUser = userRepository.findByEmail(request.getEmail());
         if ((foundUser.isPresent()) &&
-            (request.getPassword().equals(foundUser.get().getPassword()))) {
-                return new UserDTO(foundUser.get().getEmail(), foundUser.get().getUsername());
+            (passwordEncoder.matches(request.getPassword(), foundUser.get().getPassword()))) {
+                return new UserDTO(foundUser.get().getId(), foundUser.get().getEmail(), foundUser.get().getUsername());
         } else {
-            return null;
+            throw new UserNotRegisteredException("User not found");
         }
     }
 
-    public UserAccount getUserById(Long id) {
-        if (userRepository.findById(id).isPresent()) {
-            return userRepository.findById(id).get();
-        } else {
-            return null;
-        }
-
-    }
 }
