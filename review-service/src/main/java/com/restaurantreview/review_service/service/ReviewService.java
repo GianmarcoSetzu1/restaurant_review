2ed2018f3a2f4a0d9e7b8d1ef6e8ced337ebda04
GianmarcoSetzu1 <gianmarco.setzu@yahoo.it>
2025-06-15 12:36:54 +0200

    [REVIEW-SERVICE] - Add DELETE review endpoint (bonus)

--- review-service/src/main/java/com/restaurantreview/review_service/service/ReviewService.java
+++ review-service/src/main/java/com/restaurantreview/review_service/service/ReviewService.java
@@ -1,8 +1,10 @@
 package com.restaurantreview.review_service.service;
 
 import com.restaurantreview.review_service.dto.ReviewCreationRequest;
+import com.restaurantreview.review_service.exception.UserNotOwnerException;
 import com.restaurantreview.review_service.model.Review;
 import com.restaurantreview.review_service.repository.ReviewRepository;
+import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,6 +24,12 @@ public class ReviewService {
     reviewRepository.save(newReview);
 
     return newReview;
+  }
+
+  public void deleteReview(Long userId, Long reviewId) throws NoSuchElementException {
+    Review foundReview = reviewRepository.findById(reviewId).orElseThrow();
+    if (!foundReview.getUserId().equals(userId)) throw new UserNotOwnerException();
+    reviewRepository.delete(foundReview);
   }
 
   public Page<Review> findReviews(Pageable pageable) {
